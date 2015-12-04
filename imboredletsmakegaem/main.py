766bee527592ba6fd9592a0c9782aedfb05644cb
Lily Lemmer <lily.lemmer@ave81.com>
2015-12-03 20:12:44 -0600

    Add two Character constructors: random, from_assigned
    Replace `create_baddie()` function with a `random()`
    `classmethod`, i.e., `Character.random()`.
    
    Change `Character` constructor so it no longer
    handles creating a `Character` from distributed
    stats (`assign_points()`). The aforementioned
    is deprecated in favor of a `from_assigned()`
    `classmethod`, i.e., `Character.from_assigned()`.
    
    Style changes, docstrings, comments.

--- imboredletsmakegaem/main.py
+++ imboredletsmakegaem/main.py
@@ -8,10 +8,14 @@ class Character(object):
     """Should be able to use this class for both
     enemies *and* the human player.
 
+    Example:
+        >>> derp = Character.random('goblin', level=5)
+        >>> derp = Character('human', attack=44, ...)
+
     """
 
     def __init__(self, name, attack=1, defense=1, agility=1,
-                 luck=1, hitpoints=10, level=0, kills=0, assign=0):
+                 luck=1, hitpoints=10, level=0, kills=0):
 
         """
 
@@ -30,8 +34,64 @@ class Character(object):
         self.kills = kills
 
         self.max_hitpoints = hitpoints
-        
-        if assign > 0: self.assign_points(assign)
+
+        self.stat_labels = {
+                            "ATK": "attack",
+                            "DEF": "defense",
+                            "AGL": "agility",
+                            "LCK": "luck"
+                           }
+
+    @classmethod
+    def from_assigned(cls, name, points_to_assign):
+        """Create a character object, by interactively
+        promting the user to distribute n points among
+        their stats.
+
+        Args:
+            name (str): The name of this character, e.g.,
+                "Cool Girl the Awesome."
+            points_to_assign (int): Number of points to
+                interactively distribute into stats.
+
+        Returns:
+            Character: Whose stats are created from
+                character.assign_points().
+
+        Notes:
+            Another good name would be "from_interactive."
+
+        """
+
+        character = cls(name)
+        character.assign_points(points_to_assign)
+
+        return character
+
+    @classmethod
+    def random(cls, name, level):
+        """Create an character instance of "name"
+        with randomly generated stats.
+
+        Returns:
+            Character: --
+
+        """
+
+        # generate some random stats
+        # use this class' init function
+        # to produce and return a character
+        level = level
+        attack = randint(0,2 + level)
+        defense = randint(0,2 + level)
+        agility = randint(0,2 + level)
+        luck = randint(0,2 + level)
+        hitpoints = randint(3, 4 + level)
+
+        character = cls(name, level, attack, hitpoints,
+                        defense, agility, luck)
+
+        return character
 
     def assign_points(self, points):
         """Namely for HumanPlayer to distribute
@@ -43,14 +103,11 @@ class Character(object):
 
         """
 
-        stats = {
-            "ATK": "attack",
-            "DEF": "defense",
-            "AGL": "agility",
-            "LCK": "luck"
-            }
-
         while points > 0:
+            # NOTE:
+            # use textwrap.dedent with a ''' string
+            # i personally like to use ''' for non-docstring multi
+            # line strings
             print "\nYou have %s points remaining" % points
             print "Assign points by typing in the stat you want to boost.\n"
             print "ATK: %s" % self.attack
@@ -59,15 +116,14 @@ class Character(object):
             print "LCK: %s \n" % self.luck
             selection = raw_input("$").upper()
             
-            if selection in stats:
+            if selection in self.stat_labels:
                 # Increase the selected stat by one
-                setattr(self, stats[selection], 
-                        getattr(self, stats[selection]) + 1)
+                setattr(self, self.stat_labels[selection], 
+                        getattr(self, self.stat_labels[selection]) + 1)
                 points -= 1
 
             else:
                 print "\nTry Again.\n"
-
 
     def level_up(self):
         """Checks if player can level up.
@@ -81,7 +137,6 @@ class Character(object):
 
         else:
             return None
-
 
     def print_stats(self):
         """Print the current stats of the player 
@@ -102,38 +157,29 @@ class Character(object):
         for stat, name in order.iteritems():
             print "%s: %s" % (name, getattr(self, stat))
     
-
     def punch(self, defender):
+        """Attack the defender!
+
+        This object will deal damage to the defender.
+
+        Explain stat damage here.
+
+        Args:
+            defender (Character): --
+
+        """
 
         attack_roll = randint(0, self.luck) + self.attack
         defend_roll = randint(0, defender.luck / 2) + defender.defense
         
         damage = attack_roll - defend_roll
 
-        if damage < 0: damage = 0
+        if damage < 0:
+            damage = 0
 
         defender.hitpoints -= damage #apply damage to defender
 
         print "\n%s hit %s for %s damage!" % (self.name, defender.name, damage)
-        
-
-def create_baddie(level):
-    """This creates a character object with 
-    semi-stats for the player to fight
-
-    """
-
-    name = "Goblin"
-    level = level
-    attack = randint(0,2 + level)
-    defense = randint(0,2 + level)
-    agility = randint(0,2 + level)
-    luck = randint(0,2 + level)
-    hitpoints = randint(3, 4 + level)
-
-    baddie = Character(name, level, attack, hitpoints, defense, agility, luck)
-
-    return baddie
 
 
 def battle(player, baddy):
@@ -202,30 +248,12 @@ def battle(player, baddy):
 
 
 def main():
-
+    # NOTE: this is a tenary
     os.system('clear' if os.name == 'posix' else 'cls')
     
     name = raw_input("What's your name? ")
-    player = Character(name, assign=8)
-    """
-    enemy = create_baddie(3)
+    player = Character.from_assigned(name, points_to_assign=8)
 
-    print player
-
-    print enemy
-    
-    print
-
-    player.print_stats()
-    
-    print
-
-    enemy.print_stats()
-
-    print
-
-    enemy.attack(player)
-    """
     print "\n*BOOM*"
 
     time.sleep(2)
@@ -248,8 +276,7 @@ def main():
     print "Now go fight.\n"
 
     while True:
-
-        enemy = create_baddie(randint(1, player.level + 1))
+        enemy = Character.random('goblin', player.level + 1)
         battle(player, enemy)
 
     return None
